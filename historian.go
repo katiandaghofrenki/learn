b10bb1fcb972295ea539588566f98f14ae3bea3a
katiandaghofrenki <katiandagho.frenki@gmail.com>
2024-12-07 11:55:35 +0200

    adventofcode day 6 still ongoing for task 2 but you can be tested

--- historian.go
+++ historian.go
@@ -21,7 +21,7 @@ func Historian(s string) ([][]rune, int) {
         for found {
             if x > 0 && grid[x-1][y] != '#' { // Ensure boundary check first
                 grid[x][y] = 'X'
-                x, y = x-1, y // Move to the new position
+                x = x-1 // Move to the new position
                 grid[x][y] = char
             } else if x > 0 && grid[x-1][y] == '#' {
                 grid[x][y] = '>' // Mark the final position before hitting '#'
@@ -129,3 +129,77 @@ grid, count := learn.Historian(input)
 */
 
 // part two under construction
+
+func Historian2(s string) ([][]rune, int) {
+    lines := strings.Split(s, "\n")
+    
+    grid := make([][]rune, len(lines))
+    for i, line := range lines {
+        parts := strings.TrimSpace(line)
+        grid[i] = []rune(parts)
+    }
+
+    count := 0
+
+    char := '^'
+    x, y, found := Found(grid, char)
+
+    for i := 0; i < len(grid)*len(grid[0]); i++ {
+        if found {
+            if x > 0 && grid[x-1][y] != '#' {
+                grid[x-1][y] = '|'
+                x = x - 1
+            } else if x > 0 && grid[x-1][y] == '#' && grid[x][y] != '^' {
+                grid[x][y] = '+'
+                // Fill '-' all the way to the '#'
+                if grid[x][y] == '+' && grid[x-1][y] == '#' && y+1 < len(grid[0]) && grid[x][y+1] != '#' {
+                    for y+1 < len(grid[0]) && grid[x][y+1] != '#' {
+                        grid[x][y+1] = '-'
+                        y = y + 1
+                    }
+                    // Change direction to down when hitting a '#'
+                    if y+1 < len(grid[0]) && grid[x][y+1] == '#' {
+                        grid[x][y] = '+'
+                        if grid[x][y] == '+' && grid[x][y+1] == '#' && x+1 < len(grid) && grid[x+1][y] != '#' {
+                            for x+1 < len(grid) && grid[x+1][y] != '#' {
+                                grid[x+1][y] = '|'
+                                x = x + 1
+                            }
+                            // Change direction to left when hitting a '#'
+                            if x+1 < len(grid) && grid[x+1][y] == '#' {
+                                grid[x][y] = '+'
+                                if grid[x][y] == '+' && grid[x+1][y] == '#' && y > 0 && grid[x][y-1] != '#' {
+                                    for y > 0 && grid[x][y-1] != '#' {
+                                        if grid[x][y-1] != '|' || grid[x][y-1] != '+' || grid[x][y-1] != '^' {
+                            
+                                            if grid[x][y-1] == '^' {
+                                                grid[x][y-1] = '^'
+                                                grid[x][y-2] = '0'
+                                                break
+                                            } else {
+                                                grid[x][y-1] = '-'
+                                                y = y - 1
+                                            }
+
+                                        }
+                                        
+                                    }
+                                    // Change direction to left when hitting a '#'
+                                    if y > 0 && grid[x][y-1] == '#' {
+                                        grid[x][y] = '+'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            continue
+        }
+    }
+    
+
+    
+    return grid, count // not yet done
+}
